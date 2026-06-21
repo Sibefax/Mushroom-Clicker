fddbfd96a7091c4e159763ea7d7776a6d33b08d7
Sibefax <den.pyatachkov@gmail.com>
2026-06-21 10:28:31 +0300

    Shorter method improve

--- Assets/Scripts/GlobalMethods.cs
+++ Assets/Scripts/GlobalMethods.cs
@@ -1,20 +1,22 @@
-public static class GlobalMethods
-{
-    public static string MakeShorter(long number)
-    {
-        string shorter;
-        string visibleNumber = number.ToString();
-        
-        if (number >= 1000000)
-        {
-            shorter = "kk";
-            visibleNumber = (number/1000000).ToString() + shorter;
-        }
-        else if (number >= 1000)
-        {
-            shorter = "k";
-            visibleNumber = (number/1000).ToString() + shorter;
-        }
-        return visibleNumber;
-    }
+public static class GlobalMethods 
+{ 
+    public static string MakeShorter(long number) 
+    { 
+        long absoluteNumber = System.Math.Abs(number);
+        string suffix = "";
+        double value = number;
+
+        if (absoluteNumber >= 1000000) 
+        { 
+            suffix = "kk";
+            value = (double)number / 1000000; 
+        } 
+        else if (absoluteNumber >= 1000) 
+        { 
+            suffix = "k"; 
+            value = (double)number / 1000; 
+        } 
+
+        return value.ToString("0.##") + suffix; 
+    } 
 }
